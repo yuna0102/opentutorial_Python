a5b4c709471fb6621f06c802b18eeda42bfceaaf
unknown <yuna0102>
2020-10-09 21:17:42 +0900

    [python] mkedir list modified

--- index.py
+++ index.py
@@ -5,10 +5,11 @@ import cgi, os
 
 files = os.listdir(path='data') #'data'라는 폴더 안에 있는 파일들을 리스트로 만들어서 files라는 변수에 저장함
 print(files) #즉, files는 리스트임.
-listStr = ''
+listStr = '' #listStr이라는 빈 변수를 만들어
 for item in files: #files 안에 있는 element들을 한 개씩 item이라는 변수에 꺼낸다.
-  listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+  listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item) #listStr = liStr + item 이었는데 이 item을 바뀌도록 한 것임
 print(listStr)
+
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
